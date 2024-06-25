54994985a9e6a1ad264a8911580b122eb2ed3f2f
tayyara-ossama <tayyara.ossama@gmail.com>
2024-06-25 17:07:04 +0300

    push every thing

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -11,8 +11,7 @@ import { fetchRevenue } from '@/app/lib/data';
 // https://airbnb.io/visx/
 
 
-export default async function RevenueChart() {
-  const revenue= await fetchRevenue();
+export default async function RevenueChart({revenue}: {revenue: Revenue[]}) {
   const chartHeight = 350;
   // NOTE: comment in this code when you get to this point in the course
 
